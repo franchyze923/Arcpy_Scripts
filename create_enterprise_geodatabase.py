9a0972505be9f458a6a2d7e8a6369c3185e67d87
Fran Polignano <franchyze923@gmail.com>
2021-03-13 10:02:14 -0500

    Update create_enterprise_geodatabase.py

--- create_enterprise_geodatabase.py
+++ create_enterprise_geodatabase.py
@@ -10,9 +10,9 @@ def create_enterprise_database(database_location, db_user):
     arcpy.management.CreateEnterpriseGeodatabase("PostgreSQL", "localhost,5433", f"{db_name}", "DATABASE_AUTH","postgres", "pwd", "SDE_SCHEMA", "sde", "pwd", '', r"C:\Program Files\ESRI\License10.8\sysgen\keycodes")
     print("Creating Super User Database Connection")
     super_user_connection_file = arcpy.management.CreateDatabaseConnection(os.path.dirname(database_location), f"{db_name}_postgres", "POSTGRESQL", "localhost,5433", "DATABASE_AUTH", "postgres", "pwd", "SAVE_USERNAME", f"{db_name}", '', "TRANSACTIONAL", "sde.DEFAULT", None)
-    print("Creating new ASP user")
+    print("Creating new user")
     arcpy.management.CreateDatabaseUser(super_user_connection_file, "DATABASE_USER", "user", "pwd", '', '')
-    print("Create ASP database connection file")
+    print("Create new user database connection file")
     user_connection_file = arcpy.management.CreateDatabaseConnection(os.path.dirname(database_location), f"{db_name}_{db_user}", "POSTGRESQL", "localhost,5433", "DATABASE_AUTH", f"{db_user}", "pwd", "SAVE_USERNAME", f"{db_name}", '', "TRANSACTIONAL", "sde.DEFAULT", None)
     print("Done\n")
     return user_connection_file
